d17785f96efe9580b31277026357050796c48798
Ugni Blanc <ajo2185@gmail.com>
2020-05-23 14:06:03 -0700

    fixing firebase login and sign up

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("");
+var routes = require("/routes/api-routes.js");
 
 app.use(routes);
 
