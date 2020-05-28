a4fc3538b37991775495b5cc9328d47d26527f8f
Cameronmbell99 <cameronbellbchs@gmail.com>
2020-05-27 21:39:14 -0700

    updated run sequelize

--- models/groups.js
+++ models/groups.js
@@ -1,26 +1,27 @@
 module.exports = function (sequelize, DataTypes) {
     var Group = sequelize.define("Group", {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-              len: [1]
-            }
-          },
-          //somehow allow the creator to add particpants to the group
-        participants: {
-            type: DataTypes.ARRAY(DataTypes.STRING),
-            validate: {
-              len: [1]
-            }
+      name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [1]
+          }
         },
-
+        //somehow allow the creator to add particpants to the group
+      participants: {
+          type: DataTypes.STRING,
+          get: function() {
+            return JSON.parse(this.getDataValue("participants"));
+          }, 
+          set: function(val) {
+              return this.setDataValue("participants", JSON.stringify(val));
+          }
+      }
     });
 
     Group.associate = function(models){
       //each group has many users
       models.Group.hasMany(models.User);
     }
-
     return Group;
 }
